2637788ad5d8565f15455c48fa82ff4820d886c4
/__Endrigo__ <113954684+Endrigogustavo@users.noreply.github.com>
2023-03-30 13:31:45 -0300

    Update Principal.java

--- ETEC/2 ano/DS/Fevereiro/Viagem/src/main/java/com/mycompany/viagem/Principal.java
+++ ETEC/2 ano/DS/Fevereiro/Viagem/src/main/java/com/mycompany/viagem/Principal.java
@@ -4,6 +4,8 @@
  */
 package com.mycompany.viagem;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Admin
@@ -13,10 +15,39 @@ public class Principal {
     public static void main(String[] args) {
         Percurso per = new Percurso();
         Custo c = new Custo();
+        int op;
+        do{
+        op = Integer.parseInt(JOptionPane.showInputDialog("Digite: \n 1-Cadastro \n 2-Lista \n 3-Calculo\n 0-Sair"));
         
-        per.CadastrarPercurso();
-        per.ListarPercurso();
-        c.calcularViagem(per);
+        switch (op) {
+            case 1:
+            per.CadastrarPercurso();
+            break;
+                
+            case 2:
+               per.ListarPercurso();
+            break;
+               
+            case 3:
+             c.calcularViagem(per);
+            
+            break;
+                
+            case 0:
+            JOptionPane.showMessageDialog(null, "Encerrando operaçao");
+            break;
+            
+            default:
+                JOptionPane.showMessageDialog(null, "Opçao invalida");
+      
+            
+        }
+        } while(op !=0);
+        
+    }
+       
+        
+        
     }
     
-}
+
