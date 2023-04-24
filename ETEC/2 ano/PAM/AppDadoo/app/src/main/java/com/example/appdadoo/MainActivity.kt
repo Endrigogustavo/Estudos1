4b442018eda68d8d04e6b3ae1a46c7995fa043ed
Endrigogustavo <endrigogustavo2006@gmail.com>
2023-04-24 16:01:24 -0300

    m

--- ETEC/2 ano/PAM/AppDadoo/app/src/main/java/com/example/appdadoo/MainActivity.kt
+++ ETEC/2 ano/PAM/AppDadoo/app/src/main/java/com/example/appdadoo/MainActivity.kt
@@ -6,49 +6,46 @@ import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    class MainActivity : AppCompatActivity() {
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_main)
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-            val btnRoll: Button = findViewById(R.id.btnRoll)
+        val btnRoll: Button = findViewById(R.id.btn)
 
-            btnRoll.setOnClickListener {
-                rollDice()
-            }
-
-        }
-
-        private fun rollDice() {
-
-
-            val diceRoll = (1..6).random()
-            val diceImage: ImageView = findViewById(R.id.imgDice)
-
-            val drawableResource = when (diceRoll) {
-                1 -> R.drawable.dice_1
-                2 -> R.drawable.dice_2
-                3 -> R.drawable.dice_3
-                4 -> R.drawable.dice_4
-                5 -> R.drawable.dice_5
-                else -> R.drawable.dice_6
-            }
-
-            diceImage.setImageResource(drawableResource)
-
-            val txtNum: TextView = findViewById(R.id.txtNum)
-            val txtRes: TextView = findViewById(R.id.txtRes)
-            var SortNum: EditText = findViewById(R.id.SortNum)
-
-            txtNum.text = diceRoll.toString()
-
-            if (diceRoll == SortNum.text.toString().toInt()) {
-                Toast.makeText(this, "Ganhou!!!", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this, "Perdeu!!!", Toast.LENGTH_SHORT).show()
-            }
-
+        btnRoll.setOnClickListener {
+            rollDice()
         }
 
     }
+
+    private fun rollDice() {
+
+
+        val diceRoll = (1..6).random()
+        val diceImage: ImageView = findViewById(R.id.img)
+
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+        val txtN: TextView = findViewById(R.id.txtN)
+        val txtR: TextView = findViewById(R.id.txtR)
+        val txtRR: TextView = findViewById(R.id.txtR)
+        var S: EditText = findViewById(R.id.S)
+        txtN.text = "O numero sorteado é "+diceRoll.toString()
+        if(diceRoll == S.text.toString().toInt()){
+            txtRR.text = "GANHOU +R$1000 na conta :)"
+        }else{
+            txtRR.text = "Perdeu men -R$10000 na conta :("
+        }
+
+    }
+
 }
