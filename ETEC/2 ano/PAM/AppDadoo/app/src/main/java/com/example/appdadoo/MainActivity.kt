75d72ba871e495703bb68e57e36d83c3747a342b
Endrigogustavo <endrigogustavo2006@gmail.com>
2023-04-24 15:25:03 -0300

    M

--- ETEC/2 ano/PAM/AppDadoo/app/src/main/java/com/example/appdadoo/MainActivity.kt
+++ ETEC/2 ano/PAM/AppDadoo/app/src/main/java/com/example/appdadoo/MainActivity.kt
@@ -2,51 +2,53 @@ package com.example.appdadoo
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val rollButton: Button = findViewById(R.id.button)
-        val S: EditText = findViewById(R.id.end)
-        rollButton.setOnClickListener { rollDice() }
+    class MainActivity : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+
+            val btnRoll: Button = findViewById(R.id.btnRoll)
+
+            btnRoll.setOnClickListener {
+                rollDice()
+            }
+
+        }
+
+        private fun rollDice() {
+
+
+            val diceRoll = (1..6).random()
+            val diceImage: ImageView = findViewById(R.id.imgDice)
+
+            val drawableResource = when (diceRoll) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            }
+
+            diceImage.setImageResource(drawableResource)
+
+            val txtNum: TextView = findViewById(R.id.txtNum)
+            val txtRes: TextView = findViewById(R.id.txtRes)
+            var SortNum: EditText = findViewById(R.id.SortNum)
+
+            txtNum.text = diceRoll.toString()
+
+            if (diceRoll == SortNum.text.toString().toInt()) {
+                Toast.makeText(this, "Ganhou!!!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Perdeu!!!", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
     }
-
-    fun rollDice() {
-        val dice = Dice1(6)
-        val diceRoll = dice.roll(6)
-        val S: EditText = findViewById(R.id.end)
-        val txt: TextView = findViewById(R.id.txtname)
-
-        val diceImage: ImageView = findViewById(R.id.imageView)
-        val drawableResource =
-            when (diceRoll) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
-        }
-
-        val num: Int = S.toString().toInt()
-
-        if (num == diceRoll){
-            txt.text = "Voce ganhou"
-        }
-        else{
-            txt.text = "Voce Perdeu"
-        }
-
-        diceImage.setImageResource(drawableResource)
-        diceImage.contentDescription = diceRoll.toString()
-    }
-
-
 }
