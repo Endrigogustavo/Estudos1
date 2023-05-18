7191c207e59ea1ab5b5ddb5f0ee1caa3bfac91d3
Endrigogustavo <endrigogustavo2006@gmail.com>
2023-05-18 14:05:41 -0300

    m

--- ETEC/2 ano/DS/Aula-Interface/Ex3/src/main/java/inter/ex3/Ex3.java
+++ ETEC/2 ano/DS/Aula-Interface/Ex3/src/main/java/inter/ex3/Ex3.java
@@ -18,68 +18,48 @@ import java.util.*;
 
 
 public class Ex3 extends JFrame{
-JButton botao1,erro, informacao, exclamacao, pergunta, nenhum,copiar,limpar;
-String nome,texto;;
+JButton copiar,limpar;
 JLabel rotulo,rotulo2,rotulo3,rotulo4,rotulo5,rotulo6;
 JTextField texto1,texto2;
-int ds,dia,mes,ano,hh,mm,ss,h;
-Calendar data,hora;
-DecimalFormat formato;
-String diasemana[]={"Domingo","Segunda - Feira","Terça - Feira","Quarta - Feira",
-"Quinta - Feira","Sexta - Feira","Sábado"};
-String meses[]={"Janeiro","Fevereiro","Março","Abril","Maio","Junho",
-"Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"};
-Object linguagens [] = {"Java","Delphi","C++","Visual Basic"};
+JComboBox lista;
+JRadioButton tam1,tam2;
+ButtonGroup grupo;
+JCheckBox auto,bar,av;
+
+String estado[] = {"Solteiro (a)","Casado (a)","Solteiro(a)","Divorciado (a)","Viúvo (a)"};
+
+
 
 
 public Ex3(){
 super("Ex3");
 Container tela = getContentPane();
 tela.setLayout(null);
-nome = JOptionPane.showInputDialog("Entre com seu nome: ");
 rotulo = new JLabel("");
-botao1 = new JButton("Sair");
-erro = new JButton("Erro");
-informacao = new JButton("Informação");
-exclamacao = new JButton("Exclamação");
-pergunta = new JButton("Pergunta");
-nenhum = new JButton("Nenhum");
-rotulo2 = new JLabel("");
-rotulo3 = new JLabel("");
-rotulo4 = new JLabel("");
+rotulo2 = new JLabel("Interesses:");
+rotulo3 = new JLabel("Sexo");
+rotulo4 = new JLabel("Estado Civil:");
 rotulo5 = new JLabel("Nome: ");
-rotulo6 = new JLabel("Nome: ");
+rotulo6 = new JLabel("Idade: ");
 texto1 = new JTextField(20);
-texto2 = new JTextField(20);
+texto2 = new JTextField(10);
 copiar = new JButton("Copiar");
 limpar = new JButton("Limpar");
-
-//horas//
-ActionListener tarefa = (new ActionListener(){
-public void actionPerformed(ActionEvent e){
-HORAS();
-}
-});
-javax.swing.Timer time = new javax.swing.Timer(1000,tarefa);
-time.start();
-
-
-//data
-data = Calendar.getInstance();
-ds = data.get(Calendar.DAY_OF_WEEK);
-dia = data.get(Calendar.DAY_OF_MONTH);
-mes = data.get(Calendar.MONTH);
-ano = data.get(Calendar.YEAR);
-rotulo2.setText(ds+" "+dia+"/"+(mes+1)+"/"+ano);
-rotulo3.setText("Data: "+diasemana[ds-1]+", "+dia+" de "+meses[mes]+" de "+ano);
+lista = new JComboBox(estado);
+tam1 = new JRadioButton("Masculino");
+tam2 = new JRadioButton("Feminino");
+grupo = new ButtonGroup();
+grupo.add(tam1);
+grupo.add(tam2);
+auto = new JCheckBox("Negrito");
+bar = new JCheckBox("Itálico");
+av = new JCheckBox("Normal");
 
 
-//minu,maiu//
+
 copiar.addActionListener(new ActionListener(){
 public void actionPerformed(ActionEvent e){
 
-texto = texto1.getText().toUpperCase();
-texto1.setText(texto);
 texto2.setText(texto1.getText().toLowerCase());}});
 limpar.addActionListener(new ActionListener(){
 public void actionPerformed(ActionEvent e){
@@ -88,32 +68,28 @@ texto2.setText("");
 texto1.requestFocus();}});
 
 
-//posicionamento na janela//
-erro.setBounds(30,300,80,20);
-informacao.setBounds(110,300,100,20);
-exclamacao.setBounds(210,300,110,20);
-pergunta.setBounds(320,300,90,20);
-nenhum.setBounds(400,300,90,20);
-rotulo.setBounds(30,200,350,20);
-rotulo2.setBounds(30,350,280,20);
-rotulo3.setBounds(30,330,280,20);
-rotulo4.setBounds(300,330,280,20);
+lista.setBounds(120,230,120,20);
+rotulo.setBounds(30,320,350,20);
+rotulo2.setBounds(30,120,280,20);
+rotulo3.setBounds(20,90,50,20);
+rotulo4.setBounds(30,230,120,20);
 rotulo5.setBounds(20,30,50,20);
 rotulo6.setBounds(20,60,50,20);
 texto1.setBounds(60,30,180,20);
-texto2.setBounds(60,60,180,20);
-copiar.setBounds(20,130,100,20);
-limpar.setBounds(140,130,100,20);
-botao1.setBounds(320,200,90,20);//sair
-botao1.setToolTipText("Botão que finaliza a janela");
+texto2.setBounds(60,60,70,20);
+copiar.setBounds(30,270,100,20);
+limpar.setBounds(170,270,100,20);
+tam1.setBounds(70,90,100,20);
+tam2.setBounds(170,90,100,20);
+auto.setBounds(30,140,100,20);
+bar.setBounds(30,160,100,20);
+av.setBounds(30,180,100,20);
 copiar.setToolTipText("Botão que deixa a fonte maiúscula e minúscula");
 limpar.setToolTipText("Botão que limpa o texto");
-erro.setToolTipText("Botão que exibe uma messagem de erro");
-informacao.setToolTipText("Botão que exibe uma messagem de informação");
-exclamacao.setToolTipText("Botão que exibe uma messagem de exclamação");
-pergunta.setToolTipText("Botão que exibe uma messagem de pergunta");
-nenhum.setToolTipText("Botão que exibe uma messagem de nada");
-rotulo.setText("O nome digitado foi: "+nome.toUpperCase());
+rotulo.setText("Criado por Endrigo  2 ds");
+
+copiar.setMnemonic(KeyEvent.VK_F1);
+limpar.setMnemonic(KeyEvent.VK_F2);
 
 tela.add(rotulo);
 tela.add(rotulo2);
@@ -123,52 +99,16 @@ tela.add(rotulo5);
 tela.add(rotulo6);
 tela.add(texto1);
 tela.add(texto2);
-tela.add(botao1);
 tela.add(copiar);
 tela.add(limpar);
-tela.add(erro);
-tela.add(informacao);
-tela.add(exclamacao);
-tela.add(pergunta);
-tela.add(nenhum);
+tela.add(lista);
+tela.add(av);
+tela.add(auto);
+tela.add(bar);
+tela.add(tam1); tela.add(tam2);
 
 
-//popups
-erro.addActionListener(new ActionListener(){
-    public void actionPerformed(ActionEvent e){
-    JOptionPane.showMessageDialog(null,"Você escolheu erro","Mensagem de Erro",JOptionPane.ERROR_MESSAGE,null);}});
-informacao.addActionListener(new ActionListener(){ 
-    public void actionPerformed(ActionEvent e){
-JOptionPane.showMessageDialog(null,"Você escolheu informação","Mensagem de Informação",JOptionPane.INFORMATION_MESSAGE,null);}});
-exclamacao.addActionListener(new ActionListener(){ 
-    public void actionPerformed(ActionEvent e){
-JOptionPane.showMessageDialog(null,"Você escolheu exclamação","Mensagem de Exclamação",JOptionPane.WARNING_MESSAGE,null);}});
-pergunta.addActionListener(new ActionListener(){ 
-    public void actionPerformed(ActionEvent e){
-JOptionPane.showMessageDialog(null,"Você escolheu pergunta","Mensagem de Pergunta",JOptionPane.QUESTION_MESSAGE,null);}});
-nenhum.addActionListener(new ActionListener(){ 
-    public void actionPerformed(ActionEvent e){
-JOptionPane.showMessageDialog(null,"Você escolheu nenhum","Mensagem",JOptionPane.PLAIN_MESSAGE,null);}});
 
-
-//linguagem
-Object opcao2 = JOptionPane.showInputDialog(null,"Qual sua linguagem favorita?"
-,"Enquete",JOptionPane.QUESTION_MESSAGE,null,linguagens,linguagens[0]);
-JOptionPane.showMessageDialog(null,"Você escolheu: "+opcao2);
-
-
-//fechar o app
-botao1.addActionListener(new ActionListener(){
-public void actionPerformed(ActionEvent e){
-int opcao;
-Object[] botoes = {"Sim","Não"};
-opcao = JOptionPane.showOptionDialog(null,"Deseja mesmo fechar a janela?","Fechar",JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE,null,botoes,botoes[0]);
-   if (opcao==JOptionPane.YES_OPTION)
-     System.exit(0);
-   }});
-
-
-//tamanho da tela//
 setSize(540,430);
 setLocationRelativeTo(null);
 setVisible(true);
@@ -179,19 +119,5 @@ setVisible(true);
 public static void main(String args[]){
 Ex3 app = new Ex3();
 app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-}
-
-//método horas
-private void HORAS(){
-hora = Calendar.getInstance();
-hh = hora.get(Calendar.HOUR_OF_DAY);
-mm = hora.get(Calendar.MINUTE);
-ss = hora.get(Calendar.SECOND);
-formato = new DecimalFormat("00");
-rotulo4.setText("Hora: "+formatar(hh%12)+":"+formatar(mm)+":"+formatar(ss));
-}
-private String formatar(int num){
-formato = new DecimalFormat("00");
-return formato.format(num);
 }
 }
