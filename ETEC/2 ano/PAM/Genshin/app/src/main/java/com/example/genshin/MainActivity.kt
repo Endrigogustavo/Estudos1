d2fb98a20b64b1d3fb089c3838447dca666ddb3f
Endrigogustavo <endrigogustavo2006@gmail.com>
2023-05-24 14:32:34 -0300

    m

--- ETEC/2 ano/PAM/Genshin/app/src/main/java/com/example/genshin/MainActivity.kt
+++ ETEC/2 ano/PAM/Genshin/app/src/main/java/com/example/genshin/MainActivity.kt
@@ -7,23 +7,16 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.material.MaterialTheme.shapes
-import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Blue
-import androidx.compose.ui.graphics.Color.Companion.Red
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.genshin.ui.theme.GenshinTheme
-import kotlin.text.Typography
-import androidx.compose.material.MaterialTheme as MaterialTheme1
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -145,17 +138,44 @@ fun Scrolling(modifier: Modifier = Modifier) {
             )
 
         }
+        Text("Núcleo Dendro\n" +
+                "Se a duração de um Dendro Core expirar, ele explodirá, causando AoE Dendro DMG em um raio de 5m para inimigos e jogadores.\n" +
+                "\n" +
+                "Se Pyro for infligido a um Dendro Core, a reação Burgeon será acionada, que consome o Dendro Core e causa aumento de AoE Dendro DMG em um raio de 5m para inimigos e jogadores.\n" +
+                "\n" +
+                "Se o Electro for infligido a um Dendro Core, a reação Hyperbloom será acionada, que transforma o Dendro Core em um Sprawling Shot que atinge o inimigo mais próximo (ou aliado, se acionado por um inimigo/ambiente), causando aumento de AoE Dendro DMG em um raio de 1m ao atingir inimigos e jogadores.\n" +
+                "\n" +
+                "O DMG causado aos jogadores por todas as reações relacionadas a Bloom é de 5% do DMG causado aos inimigos, não importa qual parte desencadeie a reação.\n" +
+                "\n" +
+                "Burgeon e Hyperbloom podem ser ativados por um grupo diferente daquele que ativou o Bloom original, incluindo o ambiente e sua escala de dano com as estatísticas do responsável pela aplicação de Pyro ou Electro.",
+            fontWeight = FontWeight.Bold, fontSize = 13.sp,color = Color.White, modifier = Modifier .padding(20.dp),)
+
+        
+        Text("núcleo abundante\n" +
+                "Certas habilidades de personagem e buffs de eventos farão com que a reação de Bloom produza núcleos abundantes em vez de núcleos Dendro. Bountiful Cores explodem 0,4 segundos após sua criação, causando AoE Dendro DMG em um raio de 6,5 m para inimigos e jogadores. DMG dado aos jogadores é 5% do DMG dado aos inimigos.\n" +
+                "\n" +
+                "Burgeon e Hyperbloom não podem ser acionados em Bountiful Cores. Bountiful Cores compartilham o limite superior de 5 com Dendro Cores regulares." +
+                "" +
+                "Dano\n" +
+                "Observe que um alvo pode receber apenas 2 instâncias de DMG de cada reação relacionada a Bloom a cada 0,5 segundos, conforme limitado pela Sequência de Dano . Observe que as explosões do Dendro Core e as explosões do Bountiful Core também são contadas separadamente.\n" +
+                "\n" +
+                "Florescer\n" +
+                "Bloom DMG é calculado como\n" +
+                "2\n" +
+                "×\n" +
+                "Multiplicador de Nível\n" +
+                "{\\displaystyle 2\\vezes {\\text{Nível Multiplicador}}}. Os multiplicadores de nível para cada nível podem ser encontrados em Level Scaling/Reaction#Level Multiplier .\n" +
+                "\n" +
+                "Bloom DMG é afetado apenas pelo nível e Maestria Elemental do personagem que está causando a reação, e o Dendro RES do alvo . Ele ignora a DEF do alvo e não pode causar Golpes Críticos . O Elemental Mastery DMG Bonus e o multiplicador de Resistência do alvo são aplicados multiplicativamente com o Base DMG.",
+            fontWeight = FontWeight.Bold, fontSize = 13.sp,color = Color.White, modifier = Modifier .padding(20.dp),)
+
+
 
     }
-}
 
 
 
-fun MaterialTheme(){
 
-    val Red700 = Color(0xffdd0d3c)
-    val Red800 = Color(0xffd00036)
-    val Red900 = Color(0xffc20029)
 }
 
 
